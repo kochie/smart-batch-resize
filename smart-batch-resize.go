4a5fb53fba341703f777053aec8e658437c555e4
Robert Koch <robert@kochie.io>
2017-08-10 01:49:49 +1000

    Added command flags, made it more user-friendly.

--- smart-batch-resize.go
+++ smart-batch-resize.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/jpeg"
@@ -8,27 +9,47 @@ import (
 	"log"
 	"os"
 	"regexp"
-	"strconv"
-	"strings"
 
 	"github.com/artyom/smartcrop"
 	"github.com/nfnt/resize"
 )
 
 func main() {
-	files, _ := ioutil.ReadDir("./")
+	inDirPtr := flag.String("in", "./", "The input file.")
+	outDirPtr := flag.String("out", "../resized", "The output file.")
+	heightPtr := flag.Float64("height", 100, "The height of the image.")
+	widthPtr := flag.Float64("width", 100, "The width of the image.")
+
+	flag.Parse()
+
+	files, _ := ioutil.ReadDir(*inDirPtr)
 	r, _ := regexp.Compile(".jpe?g$")
-	if _, err := os.Stat("../resized"); os.IsNotExist(err) {
-		fmt.Println("resized folder does not exist, making")
-		os.Mkdir("../resized", 0777)
+
+	if _, err := os.Stat(*outDirPtr); os.IsNotExist(err) {
+		fmt.Println(*outDirPtr, "folder does not exist, making")
+		os.Mkdir(*outDirPtr, 0777)
 	}
 	for _, f := range files {
 		if r.MatchString(f.Name()) {
-			height, err := strconv.ParseFloat(os.Args[1], 64)
+			file, err := os.Open(*inDirPtr + "/" + f.Name())
 			handleError(err)
-			width, err := strconv.ParseFloat(os.Args[2], 64)
+			// decode jpeg into image.Image
+			img, err := jpeg.Decode(file)
 			handleError(err)
-			resizeImage(f.Name(), height, width)
+			file.Close()
+
+			resizedImage := resizeImage(img, *heightPtr, *widthPtr)
+
+			newImageName := r.ReplaceAllString(f.Name(), ".jpg")
+
+			fmt.Println(f.Name(), "-->", newImageName)
+
+			out, err := os.Create(*outDirPtr + "/" + newImageName)
+			handleError(err)
+			defer out.Close()
+
+			// write new image to file
+			jpeg.Encode(out, resizedImage, nil)
 		} else {
 			fmt.Println(f.Name(), "isn't a picture, skipping.")
 		}
@@ -40,43 +61,21 @@ type SubImager interface {
 	SubImage(r image.Rectangle) image.Image
 }
 
-func resizeImage(imageName string, newHeight float64, newWidth float64) {
-	r, _ := regexp.Compile(".jpe?g$")
-	// open "test.jpg"
-	file, err := os.Open(imageName)
-	handleError(err)
-
-	// decode jpeg into image.Image
-	img, err := jpeg.Decode(file)
-	handleError(err)
-	file.Close()
-
+func resizeImage(img image.Image, newHeight float64, newWidth float64) (resizedImage image.Image) {
 	croppedImage := resize.Resize(uint(newWidth), 0, img, resize.Lanczos3)
 
-	topCrop, err := smartcrop.Crop(croppedImage, int(newHeight), int(newWidth))
+	topCrop, err := smartcrop.Crop(croppedImage, int(newWidth), int(newHeight))
 	handleError(err)
 	fmt.Printf("Top crop: %+v\n", topCrop)
 
 	sub, ok := croppedImage.(SubImager)
-	if ok {
-
-	} else {
+	if !ok {
 		fmt.Println("No SubImage support")
 		os.Exit(1)
 	}
 
-	resizedImage := sub.SubImage(topCrop)
-
-	newImageName := r.ReplaceAllString(imageName, ".jpg")
-
-	fmt.Println(imageName, "-->", newImageName)
-
-	out, err := os.Create(strings.Join([]string{"../resized/", newImageName}, ""))
-	handleError(err)
-	defer out.Close()
-
-	// write new image to file
-	jpeg.Encode(out, resizedImage, nil)
+	resizedImage = sub.SubImage(topCrop)
+	return
 }
 
 func handleError(err error) {
